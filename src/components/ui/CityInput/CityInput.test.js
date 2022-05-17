c918185129c185951a59926d7a7385a0d866e55a
Jam52 <jamie.sajdak@gmail.com>
2022-05-16 20:58:07 -0400

    Refactor CityInput.tests.js

--- src/components/ui/CityInput/CityInput.test.js
+++ src/components/ui/CityInput/CityInput.test.js
@@ -2,54 +2,52 @@ import { utilRender, screen } from '../../../utils/test-utils';
 import CityInput from './CityInput';
 import forecastJson from '../Forecast/forecastRes.json';
 
-describe('CityInput Component ', () => {
-  let container = null;
-  let searchedCitiesHeader = null;
-  let submitButton = null;
-  let cityButton = null;
+describe('CityInput Component', () => {
+  const setup = (state) => utilRender(<CityInput />, state);
 
   describe('Before any updates to state', () => {
-    beforeEach(() => {
-      utilRender(<CityInput />);
-      container = screen.getByTestId('city');
-      searchedCitiesHeader = screen.queryByTestId('previousCites');
-      submitButton = screen.findByTestId('cityButton');
-    });
-
     it('renders correctly', () => {
+      setup();
+      const container = screen.getByTestId('city');
       expect(container).toBeTruthy();
     });
+
     it('does not container previose city header', () => {
+      setup();
+      const searchedCitiesHeader = screen.queryByTestId('previousCites');
       expect(searchedCitiesHeader).toBeFalsy();
     });
   });
 
   describe('After update to state', () => {
-    beforeEach(() => {
-      utilRender(<CityInput />, {
-        preloadedState: {
-          weather: {
-            cityStatus: '',
-            searchedCities: [
-              { name: 'Toronto', data: forecastJson.daily },
-              { name: 'New York', data: forecastJson.daily },
-              { name: 'Nottingham', data: forecastJson.daily },
-            ],
-          },
+    const state = {
+      preloadedState: {
+        weather: {
+          cityStatus: '',
+          searchedCities: [
+            { name: 'Toronto', data: forecastJson.daily },
+            { name: 'New York', data: forecastJson.daily },
+            { name: 'Nottingham', data: forecastJson.daily },
+          ],
         },
-      });
-      container = screen.getByTestId('city');
-      searchedCitiesHeader = screen.getByTestId('previousCities');
-      cityButton = screen.getAllByTestId('previousCityButton');
-    });
+      },
+    };
 
     it('renders correctly', () => {
+      setup(state);
+      const container = screen.getByTestId('city');
       expect(container).toBeTruthy();
     });
+
     it('contains previose city header', () => {
+      setup(state);
+      const searchedCitiesHeader = screen.getByTestId('previousCities');
       expect(searchedCitiesHeader).toBeTruthy();
     });
+
     it('contains 3 buttons', () => {
+      setup(state);
+      const cityButton = screen.queryAllByTestId('previousCityButton');
       expect(cityButton).toHaveLength(3);
     });
   });
